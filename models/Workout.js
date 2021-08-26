15b70743dda0200f6d420e28a7e154eef1e38dba
Caitlin Swickard <caitlin6029@gmail.com>
2021-08-26 15:14:29 -0600

    built server.js

---
+++ models/Workout.js
@@ -0,0 +1,23 @@
+const { model, Schema } = require('mongoose');
+
+// new user schema
+// const workoutSchema = new Schema({
+
+//   day:
+
+//   name:
+
+//   type:
+//   weight:
+//   sets:
+//   reps:
+//   duration:
+//     // if cardio track distance traveled
+//   distance:
+
+
+// }, {
+//   timestamp: true,
+// });
+
+module.exports = model('Workout', workoutSchema);
---
+++ server.js
@@ -0,0 +1,22 @@
+// require packages
+const express = require('express');
+const mongoose = require('mongoose');
+
+const PORT = process.env.PORT || 3001;
+
+// mongoDB connection
+mongoose.connect('mongodb://localhost:27017/workoutTracker', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('Connected to DB'))
+  .catch(e => console.log(e));
+
+const app = express();
+
+// req.body middlewear
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.listen(PORT, () => console.log('PORT CONNECTED'))
